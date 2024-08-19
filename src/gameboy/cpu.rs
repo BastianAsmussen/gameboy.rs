96df0405c206cc7df64f2977af84f9edbed545a8
Bastian Asmussen <bastian@asmussen.tech>
2024-08-19 14:50:59 +0200

    feat: Implement instruction decoding and execution for CPU
    Added support for decoding and executing various CPU instructions, including arithmetic operations, increment/decrement, and conditional jumps. This enhances the CPU's ability to process and execute a wider range of instructions.

--- src/gameboy/cpu.rs
+++ src/gameboy/cpu.rs
@@ -67,3 +67,161 @@ impl Registers {
         self.e = (value & 0xFF) as u8;
     }
 }
+
+#[derive(Debug)]
+enum ArithmeticTarget {
+    A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+#[derive(Debug)]
+enum IncDecTarget {
+    BC,
+    DE,
+}
+
+#[derive(Debug)]
+enum PrefixTarget {
+    B,
+}
+
+#[derive(Debug)]
+enum JumpTest {
+    NotZero,
+    Zero,
+    NotCarry,
+    Carry,
+    Always,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Add(ArithmeticTarget),
+    Inc(IncDecTarget),
+    Rlc(PrefixTarget),
+    Jp(JumpTest),
+}
+
+impl Instruction {
+    const fn from_byte(byte: u8, is_prefixed: bool) -> Option<Self> {
+        if is_prefixed {
+            Self::from_prefixed_byte(byte)
+        } else {
+            Self::from_normal_byte(byte)
+        }
+    }
+
+    const fn from_prefixed_byte(byte: u8) -> Option<Self> {
+        match byte {
+            0x00 => Some(Self::Rlc(PrefixTarget::B)),
+            _ => None,
+        }
+    }
+
+    const fn from_normal_byte(byte: u8) -> Option<Self> {
+        let instruction = match byte {
+            0x02 => Self::Inc(IncDecTarget::BC),
+            0x13 => Self::Inc(IncDecTarget::DE),
+            _ => return None,
+        };
+
+        Some(instruction)
+    }
+}
+
+#[derive(Debug)]
+struct MemoryBus {
+    memory: [u8; 0xFFFF],
+}
+
+impl MemoryBus {
+    const fn read_byte(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+}
+
+#[derive(Debug)]
+struct Cpu {
+    registers: Registers,
+    pc: u16,
+    bus: MemoryBus,
+}
+
+impl Cpu {
+    fn step(&mut self) {
+        let mut instruction_byte = self.bus.read_byte(self.pc);
+
+        let is_prefixed = instruction_byte == 0xCB;
+        if is_prefixed {
+            instruction_byte = self.bus.read_byte(self.pc + 1);
+        }
+
+        let next_pc = Instruction::from_byte(instruction_byte, is_prefixed).map_or_else(
+            || {
+                let description = format!(
+                    "0x{}{instruction_byte:X}",
+                    if is_prefixed { "CB" } else { "" },
+                );
+
+                panic!("Unknown instruction found! ({description})")
+            },
+            |instruction| self.execute(instruction),
+        );
+
+        self.pc = next_pc;
+    }
+
+    fn execute(&mut self, instruction: Instruction) -> u16 {
+        match instruction {
+            Instruction::Add(target) => match target {
+                ArithmeticTarget::C => {
+                    let value = self.registers.c;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+
+                    self.pc.wrapping_add(1)
+                }
+                _ => self.pc,
+            },
+            Instruction::Jp(jump_test) => {
+                let should_jump = match jump_test {
+                    JumpTest::NotZero => !self.registers.f.zero,
+                    JumpTest::NotCarry => !self.registers.f.carry,
+                    JumpTest::Zero => self.registers.f.zero,
+                    JumpTest::Carry => self.registers.f.carry,
+                    JumpTest::Always => true,
+                };
+
+                self.jump(should_jump)
+            }
+            _ => self.pc,
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = did_overflow;
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+
+        new_value
+    }
+
+    fn jump(&self, should_jump: bool) -> u16 {
+        if should_jump {
+            let least_significant_byte = u16::from(self.bus.read_byte(self.pc + 1));
+            let most_significant_byte = u16::from(self.bus.read_byte(self.pc + 2));
+
+            (most_significant_byte << 8) | least_significant_byte
+        } else {
+            self.pc.wrapping_add(3)
+        }
+    }
+}
